453c0c7ffabe00ea92a5ad1e58307dc34698973e
jaden <jaden@HAL-9000>
2015-02-23 14:24:02 -0600

    Finished test class

--- lab05/src/lab05/Test.java
+++ lab05/src/lab05/Test.java
@@ -10,11 +10,12 @@ public class Test {
 
     public static void main(String[] args) {
         ArrayBag<Player> teamArray = new ArrayBag();
-        LinkedBag<Player> linkedBag = new LinkedBag();
+        LinkedBag<Player> teamLinkedList = new LinkedBag();
         long start = System.currentTimeMillis();
         Random rand = new Random();
         final int NUMBER_OF_ITEMS = 1000;
         
+	// populate the array
         for(int i = 0; i < NUMBER_OF_ITEMS; i++) {
             teamArray.add(new Player("John", "Guard", rand.nextInt(99) + 1));
         }
@@ -23,14 +24,35 @@ public class Test {
         System.out.println("The time that it took to assign " + NUMBER_OF_ITEMS
                 + " items to the array: " + elapsed);
         
+	//populate the linked list
         start = System.currentTimeMillis();
         for(int i = 0; i < NUMBER_OF_ITEMS; i++) {
-            linkedBag.add(new Player("John", "guard", rand.nextInt(99) + 1));
+            teamLinkedList.add(new Player("John", "guard", rand.nextInt(99) + 1));
         }
         end = System.currentTimeMillis();
         elapsed = end - start;
         System.out.println("The time that it took to assign " + NUMBER_OF_ITEMS
                 + " items to the linked list: " + elapsed);
         
-    
+	//remove the first item from the array
+	start = System.currentTimeMillis();
+	Player temp = teamArray.getItem(0);
+	teamArray.remove(temp);
+	end = System.currentTimeMillis();
+	elapsed = end - start;
+	System.out.println("\nThe time that it took to remove the first "
+		+ "item in the array: " + elapsed);
+	
+	//remove the first item from the linked list
+	//2nd temp variable is used to account for miniscule time taken to 
+	//allocate memory for new variable, as opposed to reassigning
+	//the temp from the last block
+	start = System.currentTimeMillis();
+	Player temp2 = teamLinkedList.getItem(0);
+	teamLinkedList.remove(temp2);
+	end = System.currentTimeMillis();
+	elapsed = end - start;
+	System.out.println("The time that it took to remove the first "
+		+ "item in the linked list: " + elapsed);
+    }
 }
